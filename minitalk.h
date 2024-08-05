5d7475511fa555d8a6913ba3ca49d548619c3694
alaakson <162610032+alaakson@users.noreply.github.com>
2024-08-05 19:43:11 +0300

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaakson <alaakson@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 13:29:02 by alaakson          #+#    #+#             */
+/*   Updated: 2024/08/02 13:29:03 by alaakson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "ft_printf/ft_printf.h"
+# include <signal.h>
+# include <signal.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+int		ft_atoi(const char *str);
+
+#endif
